bf9808c9cfe5e7b2af864027aaaa75d39609eee3
allstonfojas <32475653+allstonfojas@users.noreply.github.com>
2018-11-02 00:15:21 -0700

    Uploaded js

--- js/questionnaire.js
+++ js/questionnaire.js
@@ -30,3 +30,33 @@ $('.number-button').on('click',function() {
     console.log(clickedArray);
     
 });
+
+// FUNCTION FOR SIGN UP
+// var signUpType = "";
+
+// $('.signin-btn').on('click',function() {
+//     // get the button that was clicked
+//     var buttonClicked = $(this).html();
+//     console.log(buttonClicked);
+    
+//     // store it in signUpType
+//     signUpType = buttonClicked;
+//     console.log(signUpType);
+    
+// });
+
+// function change() {
+//   // var change = document.getElementById("check");
+//   if (signUpType.value == "Student") {
+//     document.test.elements["savereport"].value = "True";
+//     //document.test.submit();
+//   } else {
+//     change.value = "true";
+//   }
+// }
+
+// function myFunction() {
+//     var str = "Free Web Building Tutorials!";
+//     var result = str.link("https://www.w3schools.com");
+//     document.getElementById("demo").innerHTML = result;
+// }
